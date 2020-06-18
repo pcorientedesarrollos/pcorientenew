0cf0725f1bcddd5d461f675ed898bf9b91ead52d
pcoriente.desarrollos <pcoriente.desarrollos@gmail.com>
2020-06-18 16:23:40 -0500

    Prueba tabla clientes

--- cliente/src/app/app-routing.module.ts
+++ cliente/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientesComponent } from './shared/clientes/clientes.component';
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', component: DashboardComponent},
+{path: 'clientes', component: ClientesComponent},
+{path: '**', pathMatch: 'full', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+//Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ClientesComponent } from './shared/clientes/clientes.component';
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ClientesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
